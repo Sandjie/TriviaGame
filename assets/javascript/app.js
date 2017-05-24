4d2d5a6be6656d60be64350563c14726317235eb
Sandra <sandraswiat17@gmail.com>
2017-05-24 18:45:36 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,32 @@
+var questions = [{
+    question: "What is the most populated country in the world?",
+    choices: ["India", "China", "USA", "Pakistan"],
+    correctAnswer: 1
+},
+{
+    question: "What is the least populated country in the world?",
+    choices: ["Tuvalu", "Vatican City", "Greenland", "Monaco"],
+    correctAnswer: 1
+},
+{
+    question:"Which country's population has grown the most since last year?",
+    choices: ["Lebanon", "Zimbabwe", "Jordan", "Qatar"],
+    correctAnswer: 0,
+},
+{
+    question: "Which country has the lowest birth rates in the world?",
+    choices: ["Andorra", "South Korea", "Monaco", "Japan"],
+    correctAnswer: 2,
+}]
+var timer;
 
+var game ={
 
-function TriviaQuesiton(question, choices, correctAnswer) {
-    this.question = question;
-    this.choices = choices;
-    this.correctAnswer = correctAnswer;
+correct: 0,
+incorrect: 0,
+counter:160
 }
+
+
+
+

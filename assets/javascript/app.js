5400ef2e9c3927cc9c1455747aeb59e4484a1b2e
Sandra <Sandra@Sandras-MacBook-Air.local>
2017-05-26 11:59:51 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,68 @@
-
-
-// selecting game-area div/container to place javascript in.
-var panel = $("#game-area");
-
-
+var panel = $("#quiz-area");
+// Question set
 var questions = [{
-    question: "What is the most populated country in the world?",
-    choices: ["India", "China", "USA", "Pakistan"],
-    correctAnswer: 1
-},
-  {
-    question: "What is the least populated country in the world?",
-    choices: ["Tuvalu", "Vatican City", "Greenland", "Monaco"],
-    correctAnswer: 1
-},
-  {
-    question:"Which country's population has grown the most since last year?",
-    choices: ["Lebanon", "Zimbabwe", "Jordan", "Qatar"],
-    correctAnswer: 0,
-},
-  {
-    question: "Which country has the lowest birth rates in the world?",
-    choices: ["Andorra", "South Korea", "Monaco", "Japan"],
-    correctAnswer: 2,
+  question: "What is the most populated country in the world?",
+  answers: ["India","China", "USA", "Pakistan"],
+  correctAnswer: "China"
+}, {
+  question: "What is the least populated country in the world?",
+  answers: ["Tuvalu", "Vatican City", "Greenland", "Monaco"],
+  correctAnswer: "Vatican City"
+}, {
+  question: "Which country has the lowest birth rates in the world?",
+  answers: ["Andorra", "South Korea", "Monaco", "Japan"],
+  correctAnswer: "Monaco"
+}, {
+  question: "Which country's population has grown the most since last year?",
+  answers: ["Lebanon", "Zimbabwe", "Jordan", "Qatar"],
+  correctAnswer: "Lebanon"
+}, {
+  question: "Which country in the world is most ethically diverse by population?",
+  answers: ["United States", "Peru", "England", "Denmark"],
+  correctAnswer: "United States"
+}, {
+  question: "Which is the second country in the world that is most ethically diverse by population?",
+  answers: ["United States", "Peru", "England", "Denmark"],
+  correctAnswer: "Peru"
+}, {
+  question: "Which island is the most diverse?",
+  answers: ["Dominican Republic", "Bahamans", "Curaco", "Haiti"],
+  correctAnswer: "Curaco"
+}, {
+  question: "Which country is the most linguistically diverse?",
+  answers: ["Papuea New Guinea", "Vanatu", "Solomon Islands", "Tanzania"],
+  correctAnswer: "The BoogeyMan"
 }];
-
+// Variable that will hold the setInterval
 var timer;
 
 var game = {
-
-correct: 0,
-incorrect: 0,
-counter:160,
-
-countdown: function(){
+  correct: 0,
+  incorrect: 0,
+  counter: 160,
+  countdown: function() {
     game.counter--;
     $("#counter-number").html(game.counter);
     if (game.counter === 0) {
-        console.log("Your out of time");
-        game.done();
+      console.log("TIME UP");
+      game.done();
     }
-
-},  
-
- start: function() {
+  },
+  start: function() {
     timer = setInterval(game.countdown, 1000);
-
-   $("#smaller-container").prepend("<h2>Time Ends in.. : <span id='counter-number'>120</span> Seconds</h2>");
-
-   $("#start").remove();
-
-   for (var i = 0; i < questions.length; i++) {
+    $("#sub-container").prepend("<h2>Time Ends in.. : <span id='counter-number'>120</span> Seconds</h2>");
+    $("#start").remove();
+    for (var i = 0; i < questions.length; i++) {
       panel.append("<h2>" + questions[i].question + "</h2>");
-      for (var j = 0; j < questions[i].correctAnswer.length; j++) {
+      for (var j = 0; j < questions[i].answers.length; j++) {
         panel.append("<input type='radio' name='question-" + i +
-        "' value='" + questions[i].correctAnswer[j] + "''>" + questions[i].correctAnswer[j]);
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
       }
     }
-
-   panel.append("<button id='done'>Done</button>");
+    panel.append("<button id='done'>Done</button>");
   },
-
- done: function() {
-
-   $.each($("input[name='question-0']:checked"), function() {
+  done: function() {
+    $.each($("input[name='question-0']:checked"), function() {
       if ($(this).val() === questions[0].correctAnswer) {
         game.correct++;
       }
@@ -71,8 +70,7 @@ countdown: function(){
         game.incorrect++;
       }
     });
-
-   $.each($("input[name='question-1']:checked"), function() {
+    $.each($("input[name='question-1']:checked"), function() {
       if ($(this).val() === questions[1].correctAnswer) {
         game.correct++;
       }
@@ -80,8 +78,7 @@ countdown: function(){
         game.incorrect++;
       }
     });
-
-   $.each($("input[name='question-2']:checked"), function() {
+    $.each($("input[name='question-2']:checked"), function() {
       if ($(this).val() === questions[2].correctAnswer) {
         game.correct++;
       }
@@ -89,8 +86,7 @@ countdown: function(){
         game.incorrect++;
       }
     });
-
-   $.each($("input[name='question-3']:checked"), function() {
+    $.each($("input[name='question-3']:checked"), function() {
       if ($(this).val() === questions[3].correctAnswer) {
         game.correct++;
       }
@@ -98,8 +94,7 @@ countdown: function(){
         game.incorrect++;
       }
     });
-
-   $.each($("input[name='question-4']:checked"), function() {
+    $.each($("input[name='question-4']:checked"), function() {
       if ($(this).val() === questions[4].correctAnswer) {
         game.correct++;
       }
@@ -107,8 +102,7 @@ countdown: function(){
         game.incorrect++;
       }
     });
-
-   $.each($("input[name='question-5']:checked"), function() {
+    $.each($("input[name='question-5']:checked"), function() {
       if ($(this).val() === questions[5].correctAnswer) {
         game.correct++;
       }
@@ -116,8 +110,7 @@ countdown: function(){
         game.incorrect++;
       }
     });
-
-   $.each($("input[name='question-6']:checked"), function() {
+    $.each($("input[name='question-6']:checked"), function() {
       if ($(this).val() === questions[6].correctAnswer) {
         game.correct++;
       }
@@ -125,8 +118,7 @@ countdown: function(){
         game.incorrect++;
       }
     });
-
-   $.each($("input[name='question-7']:checked"), function() {
+    $.each($("input[name='question-7']:checked"), function() {
       if ($(this).val() === questions[7].correctAnswer) {
         game.correct++;
       }
@@ -134,34 +126,23 @@ countdown: function(){
         game.incorrect++;
       }
     });
-
-   this.result();
-
- },
-
- result: function() {
-
-   clearInterval(timer);
-
-   $("#smaller-container h2").remove();
-
-   panel.html("<h2>Your Results.</h2>");
+    this.result();
+  },
+  result: function() {
+    clearInterval(timer);
+    $("#sub-container h2").remove();
+    panel.html("<h2>Your Results.</h2>");
     panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
     panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
     panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
   }
 };
-
 // CLICK EVENTS
-
 $(document).on("click", "#start", function() {
   game.start();
-  
-
  
+  
 });
-
-
 $(document).on("click", "#done", function() {
   game.done();
   
